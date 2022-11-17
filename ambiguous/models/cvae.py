8152d1f27abf6b7bd358b148b2aee5ccd8f8c60d
NizarIslah <nizar.isl17@gmail.com>
2022-11-16 21:04:35 -0500

    vae accept 1x1 conv

--- ambiguous/models/cvae.py
+++ ambiguous/models/cvae.py
@@ -237,8 +237,9 @@ class Conv_CVAE(nn.Module):
 
 class ConvolutionalVAE(nn.Module):
     def __init__(self, latent_dim=20, n_cls=10, img_size=28, in_ch=[1,32,64,128],conditional=False,
-                 kernel_size=3, stride=2, padding=1, h_dim=None, relu=False):
+                 kernel_size=3, stride=2, padding=1, h_dim=None, relu=False, req_flatten_size=None, last_layer='linear'):
         super(ConvolutionalVAE, self).__init__()
+        self.img_size = img_size
         self.latent_dim = latent_dim
         self.n_cls = n_cls
         self.conditional = conditional
@@ -248,9 +249,14 @@ class ConvolutionalVAE(nn.Module):
         kernel_size = [kernel_size]*len(in_ch)
         stride = [stride]*len(in_ch)
         padding = [padding]*len(in_ch)
-        num_layers = len(in_ch)-1
+        self.num_layers = len(in_ch)-1
         modules = []
-        for i in range(num_layers):
+        self.calculate_final_dim()
+        if req_flatten_size is not None:
+            assert self.flatten_size == req_flatten_size, "Flatten size is not equal to required flatten size"
+        self.last_layer = last_layer
+
+        for i in range(self.num_layers):
             if i == 0 and self.conditional:
                 conv0_1 = nn.Sequential(nn.Conv2d(in_ch[i], in_ch[i+1]//2, kernel_size[i], stride=stride[i], padding=padding[i]), nn.BatchNorm2d(in_ch[i+1]//2), self.activation)
                 conv0_2 = nn.Sequential(nn.Conv2d(n_cls, in_ch[i+1]//2, kernel_size[i], stride=stride[i], padding=padding[i]), nn.BatchNorm2d(in_ch[i+1]//2), self.activation)
@@ -258,27 +264,27 @@ class ConvolutionalVAE(nn.Module):
             else:
                 conv = nn.Sequential(nn.Conv2d(in_ch[i], in_ch[i+1], kernel_size[i], stride=stride[i], padding=padding[i]), nn.BatchNorm2d(in_ch[i+1]), self.activation)
             modules.append(conv)
-        modules.append(nn.Flatten())
+        
+        if self.last_layer == 'linear':
+            modules.append(nn.Flatten())
 
-        if self.h_dim is not None:
-            modules.append(nn.Linear(128*4*4, h_dim))
-        else:
-            h_dim = 128*4*4
+        h_dim = in_ch[-1]*self.flatten_size**2
+        
         self.encoder = nn.Sequential(*modules)
         
-        self.fc_mu = nn.Linear(h_dim, latent_dim)
-        self.fc_logvar = nn.Linear(h_dim, latent_dim)
+        self.fc_mu = nn.Linear(h_dim, latent_dim) if self.last_layer=='linear' else nn.Conv2d(in_ch[-1], latent_dim, kernel_size=1)
+        self.fc_logvar = nn.Linear(h_dim, latent_dim) if self.last_layer=='linear' else nn.Conv2d(in_ch[-1], latent_dim, kernel_size=1)
 
         self.fc_mu2 = nn.Sequential(
-                                    nn.Linear(latent_dim, in_ch[-1]*4*4), 
-                                    View(shape=(-1, in_ch[-1], 4, 4)), 
+                                    nn.Linear(latent_dim, in_ch[-1]*self.flatten_size**2), 
+                                    View(shape=(-1, in_ch[-1], self.flatten_size, self.flatten_size)), 
                                     self.activation
                                 )
         modules = []
-        for i in range(num_layers):
+        for i in range(self.num_layers):
             last_pad = img_size//(2**i) % 2
             scale_factor = img_size//(2**i) / (img_size//(2**(i+1)) + last_pad)
-            act_fn = nn.Identity() if i == num_layers - 1 else self.activation # sigmoid before
+            act_fn = nn.Identity() if i == self.num_layers - 1 else self.activation # sigmoid before
             if i == 0 and self.conditional:
                 upconv0 = nn.Sequential( 
                                         nn.Upsample(scale_factor=scale_factor, mode='bilinear') , 
@@ -302,6 +308,16 @@ class ConvolutionalVAE(nn.Module):
                                     )
             modules.append(upconv)
         self.decoder = nn.Sequential(*modules)
+
+    @staticmethod
+    def output_size(img_size, kernel_size, stride, padding):
+        return (img_size - kernel_size + 2*padding)//stride + 1
+
+    def calculate_final_dim(self):
+        flatten_size = self.img_size
+        for i in range(self.self.num_layers):
+            flatten_size = self.output_size(flatten_size, self.kernel_size[i], self.stride[i], self.padding[i])
+        self.flatten_size = flatten_size
 
     def reparamaterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
